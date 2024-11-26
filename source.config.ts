2fdf598e4c3f77fe236755a1722e52d86d4aa8e0
fabianhug <fabian@swellnetwork.io>
2024-11-26 01:25:43 +0100

    initial commit

--- source.config.ts
+++ source.config.ts
@@ -1,7 +1,12 @@
-import { defineDocs, defineConfig } from 'fumadocs-mdx/config';
+import { defineDocs, defineConfig } from "fumadocs-mdx/config";
+import { remarkMermaid } from "@theguild/remark-mermaid";
 
 export const { docs, meta } = defineDocs({
-  dir: 'content/docs',
+  dir: "content/docs",
 });
 
-export default defineConfig();
+export default defineConfig({
+  mdxOptions: {
+    remarkPlugins: [remarkMermaid],
+  },
+});
